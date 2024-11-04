cb2657fdd17906a064fe2b06f368c58f32c05024
Soham714 <chuadharysoham9723@gmail.com>
2024-11-03 22:41:08 -0500

    Lab 7

---
+++ 101414821-COMP3123.exec02/readme.md
@@ -0,0 +1,2 @@
+####COMP-3123 lab exercises-2
+
---
+++ labweek09/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to Fullstack Development</h1>
+        <h2>React JS Programming Week09 Lab exercise</h2>
+        <h4>101414821</h4>
+        <h5>Soham Chaudhary</h5>
+        <h6>George Brown College, Toronto</h6>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
